65430c601cc7e96c0e88082ed154eca43117a5b9
Adam P Stallard <stallard@linprog4.cs.fsu.edu>
2016-03-30 14:59:22 -0400

    added dictionary object framework

---
+++ dictionary.cpp
@@ -0,0 +1,46 @@
+#include "Dictionary.h"
+
+
+Dict Dict::Dict() {
+
+}
+
+Dict::~Dict() {
+
+}
+
+void Dict::insert(std::string, int) {
+
+}
+
+void Dict::insert(std::string) {
+
+}
+
+void Dict::remove(std::string) {
+
+}
+
+void Dict::remove(int) {
+
+}
+
+int Dict::locate(std::string) {
+
+	return 0;
+}
+
+std::string Dict::extract(int) {
+
+	return "";
+}
+
+void Dict::clear() {
+
+}
+
+void Dict::show() {
+
+}
+
+
---
+++ dictionary.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include "trie.h"
+#include "bplus.h"
+
+class Dict {
+	private:
+		Trie trie;
+		Bplus<int> bplus;
+		int size;
+
+		void insert(std::string, int);
+	public:
+		Dict Dict();
+		~Dict();
+		void insert(std::string);
+		void remove(std::string);
+		void remove(int);
+		int locate(std::string);
+		std::string extract(int);
+		void clear();
+		void show();
+};
