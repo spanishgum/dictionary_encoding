626f8dee9f76e6bec9afd7356d0dc74eb67bcf6f
Adam P Stallard <stallard@linprog4.cs.fsu.edu>
2016-04-19 17:10:21 -0400

    Edited parse_triples.py

--- parse_triples.py
+++ parse_triples.py
@@ -16,8 +16,8 @@ def enc():
 
 def get_parser():
 	p = argparse.ArgumentParser(description='N3Parser that splits triples with line breaks')
-	p.add_argument('-i', '--ifile', nargs=1, help='specify input file')
-	p.add_argument('-o', '--ofile', nargs=1, help='specify output file')
+	p.add_argument('-i', '--ifile', help='specify input file')
+	p.add_argument('-o', '--ofile', help='specify output file')
 	return p
 
 def main():
